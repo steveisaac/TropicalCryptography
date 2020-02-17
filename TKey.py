533bf28706c15caa593f0826a4633a940e319d6c
steveisaac <steve.isaac@live.co.uk>
2020-02-17 14:32:01 +0000

    Added case where nodes have same IP. Added some comments

--- TKey.py
+++ TKey.py
@@ -13,16 +13,32 @@ class TropicalPeer:
         self.mode = mode
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.exponent = generate_exponent()
+        # The below if statements select which node acts as the server based on their IP address.
         if self.IP > self.partnerIP:
-            self.m, self.h = generate_m_h()
-            self.server = True
             self.socket.bind((self.IP, self.port))
             self.socket.listen(0)
             self.client_socket, self.client_address = self.socket.accept()
+            self.m, self.h = generate_m_h()
+            self.server = True
+        # If the IP addresses are the same, whichever node began execution first should be server.
+        # This should only happen when testing with two instances on the same machine.
+        elif self.IP == self.partnerIP:
+            try:
+                self.socket.connect((self.partnerIP, self.port))
+                self.server = False
+            except ConnectionRefusedError:
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.bind((self.IP, self.port))
+                self.socket.listen(0)
+                self.client_socket, self.client_address = self.socket.accept()
+                self.m, self.h = generate_m_h()
+                self.server = True
         else:
             self.m, self.h = None, None
             self.server = False
             connected = False
+            self.client_socket = None
+            self.client_address = None
             while not connected:
                 try:
                     self.socket.connect((self.partnerIP, self.port))
@@ -32,10 +48,9 @@ class TropicalPeer:
                 except socket.error as message:
                     print("Couldn't connect to peer: " + str(message))
                     sys.exit(1)
-            self.client_socket = None
-            self.client_address = None
         self.a, self.b, self.h_exp = None, None, None
 
+    # Exchanges initial matrices M and H between nodes.
     def exchange_m_h(self):
         if self.server:
             print("Sending initial values")
@@ -47,6 +62,7 @@ class TropicalPeer:
             self.h = pickle.loads(self.socket.recv(4096))
             print("Received initial values")
 
+    # Computes A and H^exponent from M and H. Sends A to other node and receives B. (One node's A is the other's B)
     def exchange_a_b(self):
         print("Computing intermediary values")
         self.a, self.h_exp = compute_intermediaries(self.m, self.h, self.exponent, self.mode)
@@ -65,16 +81,19 @@ class TropicalPeer:
             self.socket.sendall(pickle.dumps(self.a))
         self.socket.close()
 
+    # Derives key from A, B and H^exponent
     def derive_key(self):
         print("Computing secret key")
         return compute_secret_key(self.b, self.a, self.h_exp)
         # also change random generation to use secrets module
 
 
+# Launch with arguments partnerIP
 if __name__ == "__main__":
     T = TropicalPeer(sys.argv[1], *sys.argv[3:])
     T.exchange_m_h()
     T.exchange_a_b()
+    i = 0
     try:
         output = open(sys.argv[2], "w")
         output.write(" ".join([hex(j) for i in T.derive_key() for j in i]))
