e97088beb2defbbfb6559c2597fa6247712b5e39
steveisaac <steve.isaac@live.co.uk>
2020-02-19 17:40:46 +0000

    Completely broke mode 1

--- tropical_breaker.py
+++ tropical_breaker.py
@@ -1,43 +1,83 @@
 from tropical_key_exchange import *
-from numpy import array
 
 
-def breaker(m, h, a):
+def breaker(m, h, a, mode=1, max_terms=1000):
+    if mode == 1:
+        semigroup_op = semigroup_op_1
+    elif mode == 2:
+        semigroup_op = semigroup_op_2
     mi, hi = m, h
-    m_previous = m
-    diffs_previous = []
-    break_count = 0
-    for i in range(1000):
-        mi, hi = semigroup_op_1(mi, hi, m, h)
-        diffs = [[a - b for a, b in zip(i, j)] for i, j in zip(mi, m_previous)]
-        if diffs == diffs_previous:
-            offset = i + 2
-            print(i, diffs_previous[0][0])
-            break
-            break_count += 1
-        m_previous = mi
-        diffs_previous = diffs
-    if i == 999:
-        return False
-    for j in range(30):
-        for k in range(30):
-            if diffs[j][k]:
+    diffs = [[None] for i in range(10)]
+    hi = h
+    check_next = False
+    set_break = False
+    for i in range(max_terms):
+        mi_previous = mi
+        mi, hi = semigroup_op(mi, hi, m, h)
+        for j in range(9, 0, -1):
+            # slice because otherwise diffs[j] and diffs[j-1] would become pointers to the same list
+            diffs[j] = diffs[j-1][:]
+        diffs[0] = [[a - b for a, b in zip(i, j)] for i, j in zip(mi, mi_previous)]
+        if check_next:
+            if diffs[0] == diffs[order]:
+                offset = i + 2
+
                 break
-        if diffs[j][k]:
+            else:
+                check_next = False
+        for j in range(1, 10):
+            if diffs[0] == diffs[j]:
+                offset = i + 2
+                order = j
+                for row in range(30):
+                    for column in range(30):
+                        cycle_sum = sum([diffs[k][row][column] for k in range(order)])
+                        if cycle_sum:
+                            break
+                    if cycle_sum:
+                        break
+
+                for k in range(order):
+                    if (a[row][column] - mi[row][column] - sum([diffs[(order - 1) - l][row][column] for l in range(k)])) % cycle_sum == 0:
+                        extra = k
+                        set_break = True
+                        break
+                if set_break:
+                    break
+        if set_break:
             break
-    return i, ((a[j][k] - mi[j][k]) // diffs[j][k]) + offset
+    if i == max_terms - 1:
+        print("Unbroken/n")
+        print(mi)
+        print(mi_previous)
+        for i in range(10):
+            print()
+            print(diffs[i])
+        return False
+    print((((a[row][column] - mi[row][column]) // cycle_sum) * order) + offset + extra == e1, extra)
+    return (((a[row][column] - mi[row][column]) // cycle_sum) * order) + offset + extra, offset - 2
 
 
-repeat_index = []
-for i in range(10):
-    m, h = generate_m_h(30)
-    e1, e2 = (generate_exponent() for i in range(2))
-    i1, i2 = compute_intermediaries(m, h, e1), compute_intermediaries(m, h, e2)
-    b = breaker(m, h, i1[0])
-    if b:
-        if e1 == b[1]:
-            repeat_index.append(b[0])
+repetitions = 10000
+# number of terms to break
+terms_index = []
+errors = []
+mode = 2
+for ii in range(repetitions):
+    if ii % 1000 == 0 and ii > 0:
+        print("/nIteration: ", ii, "max: ", max(terms_index),
+              "percentage broken: ", (len(terms_index) * 100) / (ii + 1), "%")
+    m, h = generate_m_h(10)
+    e1, e2 = (generate_exponent() for ii in range(2))
+    i1, i2 = compute_intermediaries(m, h, e1, mode), compute_intermediaries(m, h, e2, mode)
+    bb = breaker(m, h, i1[0], mode)
+    if bb:
+        if e1 == bb[0]:
+            terms_index.append(bb[1])
         else:
-            print(b[1], e1)
+            errors.append([m, h, e1, e2, i1, i2, bb])
+            print(bb[1], e1)
+    else:
+        errors.append((m, h, i1[0]))
 
-print("max", max(repeat_index), "length: ", len(repeat_index))
+print("max: ", max(terms_index), "percentage broken: ", (len(terms_index) * 100) / repetitions, "%")
